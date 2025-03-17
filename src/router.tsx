e7b71395195951ea7946dc76849f487cd05f83b6
Unicornelia <ez.kornelia@gmail.com>
2025-03-17 12:25:39 +0100

    feat: setup simple routing

---
+++ src/router.tsx
@@ -0,0 +1,16 @@
+import { lazy, Suspense } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+const ActivityFeed = lazy(() => import('./pages/ActivityFeed'))
+
+export default function AppRouter() {
+  return (
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<ActivityFeed />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  )
+}
